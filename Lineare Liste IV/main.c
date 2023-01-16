1b06d26fd7258fbb478c3bcc32b32a48848acfcb
jMarlonLeon <m.juntorius@icloud.com>
2023-01-16 09:22:50 +0100

    Initial Commit

---
+++ Lineare Liste IV/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Lineare Liste IV
+//
+//  Created by Marlon Juntorius on 16.01.23.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
